63ead399945df4288a339562849f12cfc0582e3d
kmajic <k_majic@yahoo.com>
2018-07-11 17:03:15 +0200

    Update install_nfs_server.sh

--- install_nfs_server.sh
+++ install_nfs_server.sh
@@ -4,12 +4,24 @@
 # This script is used to install/setup NFS clients on a fresh CentOS 7 with only the following performed:
 # yum -y update; reboot
 
-read -rp "IP of the NFS server: " choice; export NFS_IP=$choice;
 read -rp "shared folder on the NFS server (full path): " choice; export NFS_FOLDER=$choice; echo $NFS_FOLDER;
+read -rp "How many servers will be accessing (integer): " choice; export NUMBER=$choice;
+
+for i in `seq 1 $NUMBER`; do read -rp "IP of the connecting server ("$NUMBER"): " choice; export NFS_IP$NUMBER=$choice; done
 
 yum -y install nfs-utils libnfsidmap 
-systemctl enable rpcbind; systemctl start rpcbind
+
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start rpc-statd
+systemctl start nfs-idmapd
 
 mkdir $NFS_FOLDER
-echo -e "$NFS_IP:$NFS_FOLDER\t$NFS_FOLDER\t nfs \t defaults \t 0 0" >> /etc/fstab; mount -a
+chmod 777 $NFS_FOLDER
+
+for i in `seq 1 $NUMBER`; do echo -e "$NFS_FOLDER\t$NFS_IP$NUMBER(rw,sync,all_squash)" >> /etc/exports; done
+exportfs -r
+
 showmount -e [$NFS_IP]
