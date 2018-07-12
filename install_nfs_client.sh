6be2498caccb0365f0e7f67f95e9434c3cc91d38
kmajic <k_majic@yahoo.com>
2018-07-12 11:53:06 +0200

    Update install_nfs_client.sh

--- install_nfs_client.sh
+++ install_nfs_client.sh
@@ -12,4 +12,3 @@ systemctl enable rpcbind; systemctl start rpcbind
 
 mkdir $NFS_FOLDER
 echo -e "$NFS_IP:$NFS_FOLDER\t$NFS_FOLDER\t nfs \t defaults \t 0 0" >> /etc/fstab; mount -a
-showmount -e [$NFS_IP]
