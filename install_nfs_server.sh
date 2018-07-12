291cf138b7fe502835cc1cff04c130464112ddc5
kmajic <k_majic@yahoo.com>
2018-07-12 11:41:23 +0200

    Update install_nfs_server.sh

--- install_nfs_server.sh
+++ install_nfs_server.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# trying to setup an Openshift installation with 1 master, 1 NFS server for persistent storage and 3 nodes. 
-# This script is used to install/setup NFS clients on a fresh CentOS 7 with only the following performed:
+# trying to setup an Openshift installation with 1 master, 1 NFS server for persistent storage and several nodes. 
+# This script is used to install/setup the NFS server on a fresh CentOS 7 with only the following performed:
 # yum -y update; reboot
+# a separate script is used to setup NFS clients (install_nfs_client.sh)
 
 read -rp "shared folder on the NFS server (full path): " choice; export NFS_FOLDER=$choice
 read -rp "How many servers will be accessing (integer): " choice; export NUMBER=$choice
 
-for i in `seq 1 $NUMBER`; do read -rp "IP of the connecting server ("$i"): " choice; export NFS_IP$i=$choice; done
+touch /root/temp_file.ip
+echo "" > /root/temp_file.ip
+
+for i in `seq 1 $NUMBER`; do read -rp "IP of the connecting server ($i): " choice; echo $choice >> /root/temp_file.ip; done
 
 yum -y install nfs-utils libnfsidmap 
 
@@ -21,7 +25,10 @@ systemctl start nfs-idmapd
 mkdir $NFS_FOLDER
 chmod 777 $NFS_FOLDER
 
-for i in `seq 1 $NUMBER`; do echo -e "$NFS_FOLDER\t$NFS_IP$i(rw,sync,all_squash)" >> /etc/exports; done
+for i in `cat /root/temp_file.ip`; do echo -e "$NFS_FOLDER\t$i(rw,sync,all_squash)" >> /etc/exports; done
+rm -f /root/temp_file.ip
+
 exportfs -r
 
 showmount -e localhost
+echo ""
